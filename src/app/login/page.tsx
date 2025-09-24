edb4a7689cc7f6334778d69b93bb7a24ddb83650
Guilherme Hassã <91090230+guilhermehassa@users.noreply.github.com>
2025-09-23 21:53:06 -0300

    Login Page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/login";
+
+export default function LoginPage() {
+  return (
+    <section
+      className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 via-white to-blue-200"
+      >
+      <LoginForm />
+    </section>
+  );
+}
